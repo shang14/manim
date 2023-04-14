bd4484d52e4abec0e12aa4fe312e17a79d7fe77e
shang14 <shang14@gmail.com>
2023-04-14 22:15:46 +0800

    scene.py

--- project/scene.py
+++ project/scene.py
@@ -5,3 +5,22 @@ class CreateCircle(Scene):
         circle = Circle()
         circle.set_fill(PINK, opacity = 0.5)
         self.play(Create(circle))
+        
+        
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle =Circle()
+        circle.set_fill(PINK, opacity=0.5)
+        
+        square = Square()
+        square.rotate(PI / 4)
+        
+        self.play(Create(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
+        
+        
+        
+        
+    
