c6804c5ebe61bba1225bd048a2f7a7309a07dcf5
shang14 <shang14@gmail.com>
2023-05-08 23:01:52 +0800

    project/scene.py

--- project/scene.py
+++ project/scene.py
@@ -52,4 +52,23 @@ class DifferentRotations(Scene):
         self.wait()
         
         
+        
+class ExampleRotation(Scene):
+    def construct(self):
+        self.camera.background_color = WHITE
+        m1a = Square().set_color(RED).shift(LEFT)
+        m1b = Circle().set_color(RED).shift(LEFT)
+        m2a = Square().set_color(BLUE).shift(RIGHT)
+        m2b = Circle().set_color(BLUE).shift(RIGHT)
+        
+        
+        #points = m2a.points
+        #points = np.roll(points, int(len(points)/4), axis=0)
+        #m2a.points = points
+       
+        self.play(Transform(m1a, m1b), Transform(m2a, m2b), run_time = 1)
+        
+        
+        
+        
                 
