b4580a82acef46afc563278468f2c2c3ccfbf68f
drkooo <admin@drkooo.com>
2023-06-07 13:04:01 +0800

    project/addtext.py

--- project/addtext.py
+++ project/addtext.py
@@ -1,11 +1,18 @@
 from manim import *
 
 class add(Scene):
+    
+    
     def construct(self):
-        title = Text("Add, Remove, Wait, and Play")
+        text_parameters = {
+            "color" : RED,
+            "opacity": 0.7,
+        }
+        
+        title = Text("Add, Remove, Wait, and Play", **text_parameters)
         self.add(title)
         self.wait(3)
         self.remove(title)
         self.wait(2)
-        self.play(FadeIn(title))
+        self.play(FadeIn(title)) 
         self.wait()
