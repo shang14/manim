8a315dc6024f3a6424d01b827b619927086da2fb
drkooo <admin@drkooo.com>
2023-06-08 14:55:51 +0800

    someshapes.py

--- project/someshapes.py
+++ project/someshapes.py
@@ -8,10 +8,10 @@ class someShapes(Scene):
         self.play(FadeOut(title))
         
         theShapes = []
-        theShapes.append( Square().shift(RIGHT*4) )
+        theShapes.append( Square(fill_opacity=1).shift(RIGHT*4).set_color(GREEN) )
         theShapes.append( Dot().shift(LEFT*4) )
-        theShapes.append( Circle().shift(DOWN*2))
-        theShapes.append( Rectangle().shift(UP*2))
+        theShapes.append( Circle(fill_opacity=1).shift(DOWN*2).set_color(BLUE))
+        theShapes.append( Rectangle(width=0.2, height=0.5, fill_opacity=0.5).shift(UP*2).set_color(ORANGE))
         
         for i in theShapes:
             self.play(FadeIn(i))
